e61fc41620ee7ba9e4a3752e1952e86708d53384
oliverliu0 <147303493+oliverliu0@users.noreply.github.com>
2024-10-12 16:12:07 -0700

    Deposit.java
    Added Requires, Modifies, Effects

--- DataAbstractionAssignment/src/Deposit.java
+++ DataAbstractionAssignment/src/Deposit.java
@@ -5,11 +5,19 @@ public class Deposit {
     private Date date;
     private String account;
 
+    //Requires: we require double type amount, Date type data, and String type account
+    //Modifies: this
+    //Effects: we are going to create an instance of deposit class with user input information
+
     Deposit(double amount, Date date, String account){
         this.amount = amount;
         this.date = date;
         this.account = account;
     }
+
+    //Requires: we require none
+    //Modifies: this
+    //Effects: we return the deposit amount, date of the transaction, and account type.
 
     public String toString(){
         //your code here
