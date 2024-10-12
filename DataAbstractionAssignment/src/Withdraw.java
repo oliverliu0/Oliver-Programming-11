429343912bf4cdff6609cd4563444819bcce0a75
oliverliu0 <147303493+oliverliu0@users.noreply.github.com>
2024-10-12 16:13:55 -0700

    Withdraw.java
    Added requires, modifies, and effects

--- DataAbstractionAssignment/src/Withdraw.java
+++ DataAbstractionAssignment/src/Withdraw.java
@@ -5,12 +5,18 @@ public class Withdraw {
     private Date date;
     private String account;
 
+    //Requires: we require double type amount, Date type date, and String type account
+    //Modifies: this
+    //Effects: we create an instance of withdraw class with user input information
+
     Withdraw(double amount, Date date, String account){
         this.amount = amount;
         this.date = date;
         this.account = account;
     }
-
+    //Requires: we require none
+    //Modifies: this
+    //Effects: we return the withdraw amount, date of the transaction, and account type.
     public String toString(){
         //your code here
         return "Withdraw of: $" + amount + " Date: " + date + " from account: " + account;
